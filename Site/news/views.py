da501494939929436bcedcc52dce3edbc7b55df6
nhamelin63 <67775664+nhamelin63@users.noreply.github.com>
2022-11-26 11:16:41 +0100

    ajout de news

--- Site/news/views.py
+++ Site/news/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 from .models import news
+
 # Create your views here.
+def index(request):
+    model = news.objects.all()
+    return render(request, 'index.html', {'news': model})
