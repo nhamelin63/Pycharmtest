9f52f1704c1049b8962285917a0bb2ccd8f6909b
nhamelin63 <67775664+nhamelin63@users.noreply.github.com>
2022-11-26 11:09:23 +0100

    ajout de news

--- Site/news/admin.py
+++ Site/news/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import news
 # Register your models here.
+
+class newsAdmin(admin.ModelAdmin):
+
+    list_display = ('Titre', 'Auteur', 'pub_date')
+    search_fields = ('Titre', 'Auteur', 'pub_date')
+    list_filter = ('Titre', 'Auteur', 'pub_date')
+    date_hierarchy = 'pub_date'
+    ordering = ('-pub_date',)
+
+admin.site.register(news, newsAdmin)
--- Site/news/models.py
+++ Site/news/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from autoslug import AutoSlugField
 # Create your models here.
+class news(models.Model):
+    Titre = models.CharField(max_length=250)
+    Auteur = models.CharField(max_length=250)
+    slug = AutoSlugField(populate_from='Titre')
+    subtitle = models.CharField(max_length=250)
+    contenue = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.Titre
